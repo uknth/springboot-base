5437c9f689f216498696c5f4d5997ae60da7c5a9
Ujjwal Kanth <uknth@outlook.com>
2019-03-24 05:22:10 +0530

    feat: gitignore & classname changes

--- src/main/java/me/uknth/shared/Springboot.java
+++ src/main/java/me/uknth/shared/Springboot.java
@@ -4,8 +4,7 @@ package me.uknth.shared;
  * Hello world!
  *
  */
-public class App 
-{
+public class Springboot {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
