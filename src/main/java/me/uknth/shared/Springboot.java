3b00308a31b93f18fff0e6e9e2192289386aeb7e
Ujjwal Kanth <uknth@outlook.com>
2019-03-25 02:08:36 +0530

    feat: working web server

--- src/main/java/me/uknth/shared/Springboot.java
+++ src/main/java/me/uknth/shared/Springboot.java
@@ -1,12 +1,32 @@
 package me.uknth.shared;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class Springboot {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        SpringApplication.run(Springboot.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Beans provided by Spring Boot:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 }
